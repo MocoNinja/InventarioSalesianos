4aa7ca9068b0bf43b64590491971628a316a6737
MocoNinja <javiergonzaleztello@gmail.com>
2017-05-12 13:41:23 +0200

    FUNCIONA

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
   session_start();
-  if ($_SESSION["fallo"] == "") $_SESSION["fallo"] = "ninguno";
+  if (empty($_SESSION["fallo"]) )$_SESSION["fallo"] = "ninguno";
   $conexion = mysqli_connect("localhost", "root", "", "inventario") or die("Error en la conexión de base de datos");
 ?>
