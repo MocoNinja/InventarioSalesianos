d91725aa2912b2656044a6a4ad5d5d870edf762f
MocoNinja <javiergonzaleztello@gmail.com>
2017-05-19 13:07:33 +0200

    pusheando out of control

--- php/incidencias.php
+++ php/incidencias.php
@@ -1,39 +1,96 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Gestionar Incidencias</title>
+    <title>Gestión de usuarios</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script language="javascript">
+      function meterIncidencia()
+      {
+        // console.log(document.getElementById('insertarIncidencias').style.opacity);
+        if (document.getElementById('insertarIncidencias').style.opacity == 0)
+        {
+          document.getElementById('verIncidencias').style.opacity = "0.1";
+          document.getElementById('insertarIncidencias').style.opacity = "1";
+          // document.getElementById('insertarIncidencias').style.zIndex = "1";
+        } else {
+          document.getElementById('verIncidencias').style.opacity = "1";
+          document.getElementById('insertarIncidencias').style.opacity = "0";
+        }
+      }
+    </script>
   </head>
   <body>
-  <h2>Inserte su incidencia</h2>
-    <form name="informe" id="informe" method="post" action="insincidencia.php">
+  <img src="./img/report_management.png" alt="Meter usuario" id="add_report" width="50" onclick="meterIncidencia()"> Insertar incidencia <br/>
+    <?php
+      include("conexion.php");
+      $incidencias = "SELECT * FROM Incidencias";
+      $registros = mysqli_query($conexion,$incidencias) or die("Error en la consulta de inserción $incidencias");
+    ?>
+    <div id="insertarIncidencias" style="opacity:0;">
+     <form name="usuario" id="usuario" method="post" action="insertarUsuario.php">
       <table>
-      <tr><th>Material Afectado</th><th>Fecha de incidencia</th><th>Descripción de la incidencia</th></tr>
-      <tr>
-        <td>
+        <tr>
+          <td>Nombre Incidencia</td>
+          <td><input type="text" name="incidencia" id="incidencia"></td>
+        </tr>
+        <tr>
+          <td>Fecha Incidencia</td>
+          <td><input type="date" name="fechaIncidencia" id="fechaIncidencia"></td>
+        </tr>
         <?php
-        include("conexion.php");
-        $materialesDisponibles = "SELECT idMaterial, nombre FROM Materiales";
-        $registros = mysqli_query($conexion,$materialesDisponibles) or die("Error en la consulta de inserción $materialesDisponibles");
-        echo "<select name='materiales' id='materiales'>";
+          if ($_SESSION['rol'] != "profesor"){
+            echo '        <tr>
+          <td>Solución Incidencia</td>
+          <td><input type="text" name="solucion" id="solucion"></td>
+        </tr>
+        <tr>
+          <td>Fecha Solucion</td>
+          <td><input type="date" name="fechaSolucion" id="fechaSolucion"></td>
+        </tr>';
+          }
+        ?>
+        <tr>
+          <td colspan="2"><input type="submit" name="enviar" id="enviar"></td>
+        </tr>
+      </table>
+  </form>  
+    </div>
+    <div id="verIncidencias">
+    <table>
+      <tr>
+        <th>Nombre de la incidencia</th>
+        <th>Fecha Incidencia</th>
+        <?php
+          if ($_SESSION['rol'] != "profesor"){
+            echo '        <tr>
+          <td>Solución Incidencia</td>
+          <td>Fecha Solucion</td>';
+          }
+        ?>
+      </tr>
+      <?php
         while($linea=mysqli_fetch_array($registros))
         {
-          echo"<option value='$linea[idMaterial]'>$linea[nombre]";
+          $botonBorrar = "<img src='./img/report_kill.png' onclick='borrarUsuario()' width='25'>";
+          $incidencia = $linea['incidencia'];
+          $fechaIncidencia = $linea['fechaIncidencia'];
+          if ($_SESSION['rol'] != "profesor"){
+          $solucion = $linea['solucion'];
+          $fechaSolucion = $linea['fechaSolucion'];
+          }
         }
-        echo "</select>";
-        mysqli_close($conexion);
+        echo"<tr><td>$incidencia</td><td>$fechaIncidencia</td>";
+        if ($_SESSION['rol'] != "profesor"){
+          echo "<td>$solucion</td><td>$fechaSolucion</td>";
+        }
+         echo " <td>$botonBorrar</td></tr>";
         ?>
-        </td>
-        <td>
-        <input type="date" name="fecha" value="fecha">
-        </td>
-        <td><input type="text" name="problema" value="problema"></td>
-      </tr>
+          
+        }
+        <?php
+        mysqli_close($conexion);
+      ?>
       </table>
-
-     </form>
- 
- 
+    </div>
   </body>
 </html>
--- php/usuarios.php
+++ php/usuarios.php
@@ -27,7 +27,38 @@
       $registros = mysqli_query($conexion,$usuarios) or die("Error en la consulta de inserción $usuarios");
     ?>
     <div id="insertarUsuarios" style="opacity:0;">
-      <p>Oculto al principio</p>
+     <form name="usuario" id="usuario" method="post" action="insertarUsuario.php">
+      <table>
+        <tr>
+          <td>Nombre</td>
+          <td><input type="text" name="nombre" id="nombre"></td>
+        </tr>
+        <tr>
+          <td>Apellidos</td>
+          <td><input type="text" name="apellidos" id="apellidos"></td>
+        </tr>
+        <tr>
+          <td>Rol</td>
+          <td><select name="rol" id="rol">
+            <option value="administrador">Administrador</option>
+            <option value="sat">SAT</option>
+            <option value="Profesor">Profesor</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>Username</td>
+          <td><input type="text" name="username" id="username"></td>
+        </tr>
+        <tr>
+          <td>Contraseña</td>
+          <td><input type="text" name="password" id="password"></td>
+        </tr>
+        <tr>
+          <td colspan="2"><input type="submit" name="enviar" id="enviar"></td>
+        </tr>
+      </table>
+  </form>  
     </div>
     <div id="verUsuarios">
     <table>
@@ -37,7 +68,6 @@
         <th>Rol</th>
         <th>Usuario</th>
         <th>Contraseña</th>
-        <th>Modificar Usuario</th>
         <th>Eliminar Usuario</th>
       </tr>
       <?php
@@ -50,7 +80,8 @@
           $rol = $linea['rol'];
           $usuario = $linea['username'];
           $password = $linea['password'];
-          echo"<tr><td>$nombre</td><td>$apellidos</td><td>$rol</td><td>$usuario</td><td>$password</td><td>$botonEditar</td><td>$botonBorrar</td></tr>";
+          echo"<tr><td>$nombre</td><td>$apellidos</td><td>$rol</td><td>$usuario</td><td>$password</td>
+          <td>$botonBorrar</td></tr>";
         }
         mysqli_close($conexion);
       ?>
